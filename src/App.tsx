ca565c482922a5bade7a5a4e0452f539c487b247
Massimiliano D'Elia <massimiliano.delia@traefik.io>
2024-01-30 10:17:11 +0100

    feat: add theme property checkup

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,19 @@
 import HubButton from 'components/HubButton'
 import { darkTheme, lightTheme } from 'components/themes'
+import { useMemo } from 'react'
 import { ThemeProvider } from 'styled-components'
 
 export const App = ({ theme = 'light' }: { theme?: 'light' | 'dark' }) => {
+  const safeTheme = useMemo(() => {
+    if (!['light', 'dark'].includes(theme)) {
+      return 'light'
+    }
+    return theme
+  }, [theme])
+
   return (
-    <ThemeProvider theme={theme === 'dark' ? darkTheme : lightTheme}>
-      <HubButton theme={theme} />
+    <ThemeProvider theme={safeTheme === 'dark' ? darkTheme : lightTheme}>
+      <HubButton theme={safeTheme} />
     </ThemeProvider>
   )
 }
