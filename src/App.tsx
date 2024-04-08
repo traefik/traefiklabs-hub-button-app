7b0377c1b5aa1ea8ecb107d3e92547f0640ecb0a
gndz07 <ginadzani@gmail.com>
2024-04-08 11:21:01 +0200

    feat: remove tooltip, replace with link

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,7 @@
 import HubButton from 'components/HubButton'
-import { darkTheme, lightTheme } from 'components/themes'
-import { useMemo } from 'react'
-import { ThemeProvider } from 'styled-components'
 
-export const App = ({ theme = 'light' }: { theme?: 'light' | 'dark' }) => {
-  const safeTheme = useMemo(() => {
-    if (!['light', 'dark'].includes(theme)) {
-      return 'light'
-    }
-    return theme
-  }, [theme])
-
-  return (
-    <ThemeProvider theme={safeTheme === 'dark' ? darkTheme : lightTheme}>
-      <HubButton theme={safeTheme} />
-    </ThemeProvider>
-  )
+export const App = () => {
+  return <HubButton />
 }
 
 export default App
